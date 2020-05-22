afa3ebac29d92955b1ae7718151fffb8a7378478
leka-ren <polafort@gmail.com>
2020-05-22 18:45:08 +0300

    все в src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./index.css";
-import Api from '../js/Api.js';
-import Card from '../js/Card.js';
-import CardList from '../js/CardList.js';
-import ClearError from '../js/ClearError.js';
-import DefaultValueForm from '../js/DefaultValueForm.js';
-import FormValidator from '../js/FormValidator.js';
-import Popup from '../js/Popup.js';
-import UserInfo from '../js/UserInfo.js';
+import Api from './js/Api.js';
+import Card from './js/Card.js';
+import CardList from './js/CardList.js';
+import ClearError from './js/ClearError.js';
+import DefaultValueForm from './js/DefaultValueForm.js';
+import FormValidator from './js/FormValidator.js';
+import Popup from './js/Popup.js';
+import UserInfo from './js/UserInfo.js';
 
 const placeList = document.querySelector('.places-list');
 const addCardButton = document.querySelector('.user-info__button');
